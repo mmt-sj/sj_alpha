088811c5b54541435a745d0e9392d0e1566420b5
mmt_sj <435658081@qq.com>
2016-08-22 00:01:05 +0800

    first

--- html/js/default.js
+++ html/js/default.js
@@ -1,4 +1,4 @@
 
 //初始化脚本
-document.write('<script type="jquery-3.1.0.js" ></script>');
-document.write('<script type="../bootstrap-3.3.5-dist/js/bootstrap.js" ></script>');
+document.write('<script src="jquery-3.1.0.js" ></script>');
+document.write('<script src="../bootstrap/js/bootstrap.js" ></script>');
