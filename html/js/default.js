2779eb7d19bc00c5090cdbaf1979e970581511e9
mmt_sj <435658081@qq.com>
2016-08-22 21:20:19 +0800

    first 2016年8月22日19:58:49

--- html/js/default.js
+++ html/js/default.js
@@ -1,4 +1,5 @@
 
 //初始化脚本
-document.write('<script src="jquery-3.1.0.js" ></script>');
+document.write('<script src="../js/jquery-3.1.0.js" ></script>');
 document.write('<script src="../bootstrap/js/bootstrap.js" ></script>');
+document.write('<script src="../js/background-blur.js ></script>');
