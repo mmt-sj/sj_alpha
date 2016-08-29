b4d6c850544843fc2a2873922273ff61010c660a
ly <435658081@qq.com>
2016-08-29 15:42:01 +0800

    2016年08月29日15:39:46 加入thinkphp框架

--- html/js/default.js
+++ html/js/default.js
@@ -1,7 +1,7 @@
 
 //初始化脚本
 document.write('<script src="../js/jquery-3.1.0.js" ></script>');
-document.write('<script src="../bootstrap/js/bootstrap.js" ></script>');
+document.write('<script src="../framework/bootstrap/js/bootstrap.js" ></script>');
 document.write('<script src="../js/background-blur.js" ></script>');
 
 
